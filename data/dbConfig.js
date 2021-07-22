222883f5264550577af431bf4b2abe42c0ded354
Zachary Cooremans <zcooremans@gmail.com>
2021-07-22 15:29:18 -0500

    almost

---
+++ data/dbConfig.js
@@ -0,0 +1,7 @@
+const knex = require('knex');
+const configs = require('../knexfile');
+const env = process.env.DB_ENV || 'development';
+
+// What knex configuration is actually used?
+// That depends on the value of process.env.NODE_ENV!
+module.exports = knex(configs[env]);
